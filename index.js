54be106ea4d9b7e1fe4882bf6ebbb14fad2b6860
Renrensan <pasha.renaisan@gmail.com>
2021-11-19 12:23:50 +0800

    Add 20000 money input and 2 topping flavors

--- index.js
+++ index.js
@@ -2,52 +2,113 @@ const vendingDFA = {
     q0:{
         5000 : "q1",
         10000 : "q2",
+        20000 : 'q4',
         Vanilla : 'q0',
-        Strawberry:'q0'
+        Strawberry:'q0',
+        Chocolate :'q0',
+        Orange: 'q0',
+        Banana: 'q0'
     },
     q1:{
         5000 : "q2",
         10000 : "q3",
+        20000 : 'q5',
         Vanilla : 'q0',
-        Strawberry:'q1'
+        Strawberry:'q1',
+        Chocolate :'q1',
+        Orange: 'q1',
+        Banana: 'q1'
     },
     q2:{
         5000 : "q3",
         10000 : "q4",
+        20000 : 'q6',
         Vanilla : 'q1',
-        Strawberry : 'q0'
+        Strawberry : 'q1',
+        Chocolate :'q1',
+        Orange: 'q1',
+        Banana: 'q1'
     },
     q3:{
         5000 : "q4",
         10000 : "q5",
+        20000 : 'q7',
         Vanilla : 'q2',
-        Strawberry : 'q1'
+        Strawberry : 'q1',
+        Chocolate :'q1',
+        Orange: 'q1',
+        Banana: 'q1'
     },
     q4:{
         5000 : "q5",
         10000 : "q6",
+        20000 : 'q8',
         Vanilla : 'q3',
-        Strawberry : 'q2'
+        Strawberry : 'q2',
+        Chocolate :'q2',
+        Orange: 'q2',
+        Banana: 'q2'
 
     },
     q5:{
         5000 : "q6",
         10000 : "q6",
+        20000 : 'q9',
         Vanilla : 'q4',
-        Strawberry : 'q3'
+        Strawberry : 'q3',
+        Chocolate :'q3',
+        Orange: 'q3',
+        Banana: 'q3'
     },
     q6:{
-        5000 : "q6",
-        10000 : "q6",
+        5000 : "q7",
+        10000 : "q8",
+        20000 : 'q10',
         Vanilla : 'q5',
-        Strawberry : 'q4'
+        Strawberry : 'q4',
+        Chocolate :'q4',
+        Orange: 'q4',
+        Banana: 'q4'
+    },
+    q7:{
+        5000 : "q8",
+        10000 : "q9",
+        Vanilla : 'q6',
+        Strawberry : 'q5',
+        Chocolate :'q5',
+        Orange: 'q5',
+        Banana: 'q5'
+    },
+    q8:{
+        5000 : "q9",
+        10000 : "q10",
+        Vanilla : 'q7',
+        Strawberry : 'q6',
+        Chocolate :'q6',
+        Orange: 'q6',
+        Banana: 'q6'
+    },
+    q9:{
+        5000 : "q10",
+        Vanilla : 'q8',
+        Strawberry : 'q7',
+        Chocolate :'q7',
+        Orange: 'q7',
+        Banana: 'q7'
+    },
+    q10:{
+        Vanilla : 'q9',
+        Strawberry : 'q8',
+        Chocolate :'q8',
+        Orange: 'q8',
+        Banana: 'q8'
     },
     startState : 'q0'
 }
-const toppings = ['Vanilla', 'Strawberry'];
+const toppings = ['Vanilla', 'Strawberry','Chocolate','Orange','Banana'];
 currentState = vendingDFA.startState;
 let numOfToppings = 0;
-const maxMoneyLimit =  30000;
+const maxMoneyLimit =  50000;
 let totalMoney = 0;
 
 function getNextState(state, input){
@@ -60,11 +121,15 @@ function getNextState(state, input){
             showBoughtToppings(0)
             numOfToppings++;
             totalMoney -= 5000;
-        }else if(input == toppings[1]){
-            showBoughtToppings(1)
-            numOfToppings++;
-            totalMoney -= 10000;
         }
+        for(index = 1 ; index<=toppings.length ; index++){
+            if(input == toppings[index]){
+                showBoughtToppings(index)
+                numOfToppings++;
+                totalMoney -= 10000;
+            }
+        }
+
     };
 }
 
@@ -78,13 +143,13 @@ function nextStateCheck(currentState,formerState){
 
 function handleInput(amount){
     let input = amount;
-    let error = 'Rejected, Max Money Limit Reached'
-    if(input === 5000 || input === 10000 && totalMoney<maxMoneyLimit){
+    let error = 'Rejected, Max Money is 50000'
+    if(input === 5000 || input === 10000 || input === 20000&& totalMoney<maxMoneyLimit){
         totalMoney+=amount;
         // if limit reached cannot input more money
         if(totalMoney>maxMoneyLimit){
             totalMoney -= amount;
-            console.log(error);
+            alert(error);
             return;
         }
     }
